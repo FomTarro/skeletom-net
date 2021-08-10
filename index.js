97a76830c2b23cc5dcfea5d0a63cb1ceec1fea10
tom <twfarro@wpi.edu>
2021-08-10 08:48:35 -0400

    commented out all but the redirect

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 async function launch(){
     const port = 8080;
     const baseDirectory = path.join(__dirname, './public');
-
+    /*
     // Tells express to treat the base directory as relative to the given directory
     // IE localhost:8080/img/blush.png corresponds to public/img/blush.png
     app.use('/', express.static(baseDirectory));
@@ -14,10 +14,10 @@ async function launch(){
     app.get('/about', (req, res) => {
         res.sendFile((path.join(baseDirectory, 'about.html')))
     });
-
+    */
     // Tells the browser to redirect to the given URL
     app.get('/stream', (req, res) => {
-        res.redirect('https://twitch.tv/fomtarro');
+        res.redirect('https://twitch.tv/skeletom_ch');
     });
 
     // Makes an http server out of the express server
