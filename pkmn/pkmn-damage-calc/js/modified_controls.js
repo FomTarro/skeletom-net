dc94663391186da092aea676ca48c39296581459
Tom Farro <twfarro@wpi.edu>
2023-08-05 15:43:12 -0400

    remove header logo

--- pkmn/pkmn-damage-calc/js/modified_controls.js
+++ pkmn/pkmn-damage-calc/js/modified_controls.js
@@ -271,6 +271,6 @@ function stringToElementSelector(str, preserveCase){
 $(document).ready(function () {
     if(parent){
         document.getElementById('header').style.height = 0;
-        document.getElementById('logo').style.height
+        document.getElementById('logo').style.height = 0;
     }
 });
